5ccf08e839685d89215b3f626a33b369298eb84f
omoterikuto <omoterikuto@gmail.com>
2020-05-12 00:02:50 +0900

    5/11

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -7,27 +7,9 @@ use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-  public function index(Request $request, Response $response) 
+  public function index()
   {
-    $html = <<<EOF
-    <!DOCTYPE html>
-    <html lang="ja">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Document</title>
-    </head>
-    <body>
-    <p>$request</p>
-    <p>$response</p>
-    </body>
-    </html>
-    EOF;
-    $response->setContent($html);
-    echo $request->fullUrl();
-    // クエリ情報含む
-    echo $request->url();
-    echo $request->path();
-    return $response;
+    $data = ['msg' => 'お名前を入力してください。'];
+    return view('hello.index', $data);
   }
 }
---
+++ src/resources/views/hello/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.helloapp')
+@section('titlt', 'Index')
+@section('menubar')
+@parent
+インデックスページ
+@endsection
+
+@section('content')
+<p>本文</p>
+@endsection
+
+@section('footer')
+copyright 2020 tuyano.
+@endsection
